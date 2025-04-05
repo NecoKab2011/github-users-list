cff6556663109e3fab84cf467f98d6fa1e7e77ff
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2025-04-05 13:47:10 +0200

    add-task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 console.log("test")
+
+// Отримати список користувачів з GitHub API Опис: Використайте публічне API 
+// GitHub для отримання списку користувачів. Зробіть GET-запит за 
+// адресою https://api.github.com/users та перегляньте отримані дані щодо 
+// користувачів.
+
+fetch("https://api.github.com/users")
+.then((respons) => {
+return respons.json()
+})
+.then((data) => {
+    makeUserList(data)
+})
+.catch((error) => {
+console.log(error)
+})
+
+function makeUserList(users) {
+    console.log(users)
+
+    const elements = users.map((user) => 
+       `
+        <li class="user-item">
+            <p class="user-login">${user.login}</p>
+            <p class="user-id">${user.id}</p>
+        </li>
+    `
+    ).join("")
+
+    document.querySelector(".users-list").innerHTML = elements
+    console.log(elements)
+}
+
